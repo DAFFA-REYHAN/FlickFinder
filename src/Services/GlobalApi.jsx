840baa41063c0a0a7edd45d6e747b8bf63f6b29d
Daffa Reyhan <dffreyhan@gmail.com>
2023-11-02 22:42:39 +0700

    Add Navbar and Setup API

---
+++ src/Services/GlobalApi.jsx
@@ -0,0 +1,13 @@
+import axios from "axios"
+
+const movieBaseUrl = "https://api.themoviedb.org/3"
+const api_key = "487683bedcaa323080ef5402dd737be5"
+
+//https://api.themoviedb.org/3/movie/550?api_key=487683bedcaa323080ef5402dd737be5
+const getTrendingVideos = axios.get(
+  this.movieBaseUrl + "/trending/all/day?api_key=" + api_key
+)
+
+export default {
+  getTrendingVideos,
+}
